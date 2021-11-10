9c92be0f0b4f028eabf516b2cf9bcfe0d25395db
Ivan Popovic <ivanivopop@gmail.com>
2021-11-10 11:00:54 +0100

    mouse event changed

--- task-4/slider.js
+++ task-4/slider.js
@@ -1,99 +1,99 @@
-(function () {
-  const $ = (e) => document.querySelector(e);
-  const $$ = (e) => document.querySelectorAll(e);
+;(function () {
+   const $ = (e) => document.querySelector(e)
+   const $$ = (e) => document.querySelectorAll(e)
 
-  let gallery = $('.gallery');
-  let gallerySlide = $('.gallery-slide');
-  let images = $$('.img');
-  let header = $('.header');
+   let gallery = $('.gallery')
+   let images = $$('.img')
+   let header = $('.header')
 
-  let backBtn = $('.fa-arrow-left');
-  let nextBtn = $('.fa-arrow-right');
-  let closeBtn = $('.del-btn');
+   let backBtn = $('.fa-arrow-left')
+   let nextBtn = $('.fa-arrow-right')
+   let closeBtn = $('.del-btn')
 
-  let counter = 0;
-  let mouseState = false;
+   let counter = 0
+   let mouseState = false
 
-  backBtn.addEventListener('click', handleSlide);
-  nextBtn.addEventListener('click', handleSlide);
-  gallery.addEventListener('mouseover', setMouseState);
-  gallery.addEventListener('mouseout', setMouseState);
-  document.addEventListener('keydown', handleKeySlide);
+   backBtn.addEventListener('click', handleSlide)
+   nextBtn.addEventListener('click', handleSlide)
+   gallery.addEventListener('mouseenter', setMouseState)
+   gallery.addEventListener('mouseout', setMouseState)
+   document.addEventListener('keydown', handleKeySlide)
 
-  handleArrows();
-  startSlide();
+   handleArrows()
+   startSlide()
 
-  function startSlide() {
-    reset();
-    images[counter].style.display = 'block';
-  }
+   function startSlide() {
+      reset()
+      images[counter].style.display = 'block'
+   }
 
-  function handleSlide(e) {
-    reset();
-    e.target.classList.contains('fa-arrow-left')
-      ? (counter--, (images[counter].style.animation = '1.5s slideRight'))
-      : (counter++, (images[counter].style.animation = '1.5s slideLeft'));
-    images[counter].style.display = 'block';
-    handleArrows();
-  }
+   function handleSlide(e) {
+      reset()
+      e.target.classList.contains('fa-arrow-left')
+         ? (counter--, (images[counter].style.animation = '1.5s slideRight'))
+         : (counter++, (images[counter].style.animation = '1.5s slideLeft'))
+      images[counter].style.display = 'block'
+      handleArrows()
+   }
 
-  function setMouseState(e) {
-    mouseState = !mouseState;
-  }
+   function setMouseState(e) {
+      console.log(e)
+      mouseState = !mouseState
+   }
 
-  function handleKeySlide(e) {
-    if (mouseState) {
-      reset();
-      if (e.code === 'ArrowLeft' && counter > 0) {
-        counter--;
-        images[counter].style.animation = '1.5s slideRight';
-      } else if (e.code === 'ArrowRight' && counter < images.length - 1) {
-        counter++;
-        images[counter].style.animation = '1.5s slideLeft';
+   function handleKeySlide(e) {
+      if (mouseState) {
+         reset()
+         if (e.code === 'ArrowLeft' && counter > 0) {
+            counter--
+            images[counter].style.animation = '1.5s slideRight'
+         } else if (e.code === 'ArrowRight' && counter < images.length - 1) {
+            counter++
+            images[counter].style.animation = '1.5s slideLeft'
+         }
+         images[counter].style.display = 'block'
+         handleArrows()
       }
-      images[counter].style.display = 'block';
-      handleArrows();
-    }
-  }
+   }
 
-  function reset() {
-    images.forEach((img) => {
-      img.style.display = 'none';
-    });
-  }
+   function reset() {
+      images.forEach((img) => {
+         img.style.display = 'none'
+      })
+   }
 
-  function handleArrows() {
-    counter >= images.length - 1
-      ? (nextBtn.style.visibility = 'hidden')
-      : (nextBtn.style.visibility = 'visible');
-    counter <= 0
-      ? (backBtn.style.visibility = 'hidden')
-      : (backBtn.style.visibility = 'visible');
-  }
+   function handleArrows() {
+      counter >= images.length - 1
+         ? (nextBtn.style.visibility = 'hidden')
+         : (nextBtn.style.visibility = 'visible')
+      counter <= 0
+         ? (backBtn.style.visibility = 'hidden')
+         : (backBtn.style.visibility = 'visible')
+   }
 
-  images.forEach((image) => {
-    image.addEventListener('click', imageGrow);
-  });
+   images.forEach((image) => {
+      image.addEventListener('click', imageGrow)
+   })
 
-  function imageGrow() {
-    reset();
-    images[counter].style.display = 'block';
-    gallery.classList.add('growGallery');
-    images.forEach((img) => {
-      img.classList.add('imgGrow');
-    });
-    header.style.display = 'none';
-    closeBtn.style.display = 'inline-block';
-  }
+   function imageGrow() {
+      reset()
+      images[counter].style.display = 'block'
+      gallery.classList.add('growGallery')
+      images.forEach((img) => {
+         img.classList.add('imgGrow')
+      })
+      header.style.display = 'none'
+      closeBtn.style.display = 'inline-block'
+   }
 
-  closeBtn.addEventListener('click', closeGrow);
+   closeBtn.addEventListener('click', closeGrow)
 
-  function closeGrow() {
-    gallery.classList.remove('growGallery');
-    images.forEach((img) => {
-      img.classList.remove('imgGrow');
-    });
-    header.style.display = 'flex';
-    closeBtn.style.display = 'none';
-  }
-})();
+   function closeGrow() {
+      gallery.classList.remove('growGallery')
+      images.forEach((img) => {
+         img.classList.remove('imgGrow')
+      })
+      header.style.display = 'flex'
+      closeBtn.style.display = 'none'
+   }
+})()
